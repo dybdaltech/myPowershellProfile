975635b6b9673b7235a66c062e354bd9fefbef8c
Dybdatech <29250734+dybdaltech@users.noreply.github.com>
2018-05-29 12:54:57 +0200

    Added a bunch of new functions. Mostly for me

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,5 @@
-new-alias grep sls
-
-function make-node([string]$projectName, [string]$info){
-    mkdir $projectName
-    Set-Location $projectName
-    New-item "App.js" -type file -Confirm
-    New-item "Views" -type Directory -Confirm
-    New-Item "Models" -type Directory -Confirm
-    New-Item "public" -type Directory -Confirm
-    New-Item "info.txt" -type file 
-    Add-Content info.txt $info 
-}
-
-function ffs($program){
-    runas $program /user:<DOMAIN\USERNAME>
-}
+New-Alias hero heroku
+$env:FLASK_APP = "script.py"
 
 function prompt   
 {  
@@ -31,4 +17,86 @@ function prompt
 
 function vim($file){
     bash -c "vim $file"
+}
+
+function vhelp(){
+    Write-Host "Vue init webpack *project name*"
+    Write-Host "CD into it. And do npm run dev"
+}
+
+function boilnode($pname) {
+    git clone https://github.com/dybdaltech/node_boiler $pname
+    cd $pname
+}
+
+function help-custom () {
+    Write-Host "Func Vim <file>"
+    Write-Host "vhelp"
+    Write-Host "boilnode(<project_name>)"
+    Write-Host "Hero == Heroku"
+    Write-Host "sh-nmap"
+    Write-Host "cd-web/pro <folder>"
+}
+
+function sh-nmap(){
+    Write-Host "C:\Program Files (x86)\Nmap"
+}
+
+function flask-start(){
+    Flask run --host=0.0.0.0
+}
+
+function cd-web($projectName){
+    cd "C:\Users\[USERNAME]\Onedrive\Web\$projectName"
+}
+
+function cd-pro($projectName){
+    cd "C:\Users\[USERNAME]\Onedrive\Programming\$projectName"
+}
+
+function start-venv(){
+    ".\Scripts"
+}
+
+function find-aduser([string]$aname){
+    $aname
+    $aduser = Get-Aduser -LDAPFilter "(anr=$aname)"
+    Write-Host $aduser
+    return $aduser
+}
+
+function ..([string]$task){
+    $filePath = ([Environment]::GetFolderPath("MyDocuments")+'\'+"$env:computerName"+'Data.txt')
+    Write-Output $task | Out-File $filePath -Append
+}
+
+function rr (){
+    bash -c "vim /mnt/c/Users/[USERNAME]/OneDrive/todo.txt"
+}
+
+function get-ipInfo ($IPAdress){
+    Invoke-RestMethod -Method GET -Uri "http://api.db-ip.com/v2/free/$IPAdress"
+}
+
+function get-dnsInfo ([string]$hostname) {
+    Write-Host "$hostname Information:"
+    $dnsInfo = Resolve-DnsName $hostname
+    $adr =  $dnsInfo.IPAddress
+    $test = $adr | Select-String -Pattern "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    if ( $test.GetType().FullName -eq "System.Object[]") {
+        $adr = $adr[1]
+        Write-Host "Multiple Adresses. Using $adr"
+    } else {
+        $adr = $adr
+        Write-Host "One address found. Using $adr"
+    }
+    $pTime = (Test-Connection -ComputerName $adr -Count 4  | measure-Object -Property ResponseTime -Average).average 
+    if($pTime -lt 50 ) {
+        Write-Host "Latency: $pTime" -ForegroundColor Green
+    } elseif ($pTime -gt 51 -and $pTime -lt 100) {
+        Write-Host "Latency: $pTime" -ForegroundColor Yellow
+    } elseif ($pTime -gt 101) {
+        Write-Host "Latency: $pTime" -ForegroundColor Red
+    }
+    Invoke-RestMethod -Method GET -Uri "http://api.db-ip.com/v2/free/$adr"
 }
